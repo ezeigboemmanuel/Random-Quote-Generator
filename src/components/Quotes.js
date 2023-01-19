1c8729c55731ff55642650e4e365ecfb8a7b02b5
ezeigboemmanuel <ezeigboemmanuel2@gmail.com>
2023-01-19 12:42:02 -0600

    worked on the site

---
+++ src/components/Quotes.js
@@ -0,0 +1,26 @@
+import React from "react";
+import '../App.css';
+
+function Quote(props){
+    let randomNumber = Math.floor(Math.random() * props.data.length);
+    const [quote, setQuote] = React.useState(props.data[randomNumber]);
+
+    function handleClick(){
+        let randomNumber = Math.floor(Math.random() * props.data.length);
+        setQuote(props.data[randomNumber])
+    }
+
+    return (
+        <div id="quote-box">
+            <h1 id="text">"{quote.quote}"</h1>
+            <h4 id="author">~{quote.author}</h4>
+            <div id = "quote-div">
+                <button id="new-quote" onClick = {handleClick}>New Quote</button>
+                <a href = "" id="tweet-quote">Tweet</a>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Quote;
---
+++ src/components/QuotesData.js
@@ -0,0 +1,24 @@
+const QuotesData = [
+    {
+        "author": "Ezeigbo",
+        "quote": "Make money before you love!"
+    },
+    {
+        "author": "Ben",
+        "quote": "Enjoy the moment"
+    },
+    {
+        "author": "chris",
+        "quote": "Nothing to write"
+    },
+    {
+        "author": "Albert",
+        "quote": "Never judge"
+    },
+    {
+        "author": "Einstein",
+        "quote": "A book by its cover"
+    }
+]
+
+export default QuotesData;
